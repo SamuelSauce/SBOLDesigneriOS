09add35ea1b7de548d58f3e356164e19b1eb557d
SamuelSauce <samuelfbridge@gmail.com>
2018-11-05 15:03:47 -0700

    Now has functionality to draw, place glyphs and clear the canvas.

--- SBOLDesigner/CanvasViewController.swift
+++ SBOLDesigner/CanvasViewController.swift
@@ -12,77 +12,124 @@ class CanvasViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.PartScrollView.setupScrollView()
+        currentlySelected = PartScrollView.CDS
+        currentlySelected.backgroundColor = UIColor.blue
+        self.DrawView.setCurrentGlyph(image: (self.PartScrollView.CDS.imageView?.image)!)
     }
     
     @IBOutlet weak var PartScrollView: PartScrollView!
-    @IBOutlet weak var FirstImage: UIImageView!
+    @IBOutlet var DrawView: DrawView!
+    var currentlySelected: UIButton!
     
     @IBAction func CDS(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.CDS
         AddPart(part: (self.PartScrollView.CDS.imageView?.image)!)
     }
     @IBAction func Gen(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Gen
         AddPart(part: (self.PartScrollView.Gen.imageView?.image)!)
     }
     @IBAction func RBS(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.RBS
         AddPart(part: (self.PartScrollView.RBS.imageView?.image)!)
     }
     @IBAction func Pro(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Pro
         AddPart(part: (self.PartScrollView.Pro.imageView?.image)!)
     }
     @IBAction func Ter(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Ter
         AddPart(part: (self.PartScrollView.Ter.imageView?.image)!)
     }
     @IBAction func gRNA(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.gRNA
         AddPart(part: (self.PartScrollView.gRNA.imageView?.image)!)
     }
     @IBAction func Ori(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Ori
         AddPart(part: (self.PartScrollView.Ori.imageView?.image)!)
     }
     @IBAction func OriT(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.OriT
         AddPart(part: (self.PartScrollView.OriT.imageView?.image)!)
     }
     @IBAction func PBS(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.PBS
         AddPart(part: (self.PartScrollView.PBS.imageView?.image)!)
     }
     @IBAction func SR5(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.SR5
         AddPart(part: (self.PartScrollView.SR5.imageView?.image)!)
     }
     @IBAction func SR3(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.SR3
         AddPart(part: (self.PartScrollView.SR3.imageView?.image)!)
     }
     @IBAction func Scar(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Scar
         AddPart(part: (self.PartScrollView.Scar.imageView?.image)!)
     }
     @IBAction func Op(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Op
         AddPart(part: (self.PartScrollView.Op.imageView?.image)!)
     }
     @IBAction func BRS(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.BRS
         AddPart(part: (self.PartScrollView.BRS.imageView?.image)!)
     }
     @IBAction func OH5(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.OH5
         AddPart(part: (self.PartScrollView.OH5.imageView?.image)!)
     }
     @IBAction func OH3(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.OH3
         AddPart(part: (self.PartScrollView.OH3.imageView?.image)!)
     }
     @IBAction func PolyA(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.PolyA
         AddPart(part: (self.PartScrollView.PolyA.imageView?.image)!)
     }
     @IBAction func SRS(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.SRS
         AddPart(part: (self.PartScrollView.SRS.imageView?.image)!)
     }
     @IBAction func NGA(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.NGA
         AddPart(part: (self.PartScrollView.NGA.imageView?.image)!)
     }
     @IBAction func Sig(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.Sig
         AddPart(part: (self.PartScrollView.Sig.imageView?.image)!)
     }
     @IBAction func BS(_ sender: Any) {
+        currentlySelected.backgroundColor = UIColor.white
+        currentlySelected = PartScrollView.BS
         AddPart(part: (self.PartScrollView.BS.imageView?.image)!)
     }
     
     func AddPart(part: UIImage){
-        FirstImage.image = part
+        currentlySelected.backgroundColor = UIColor.blue
+        self.DrawView.setCurrentGlyph(image: part)
     }
 
 }
---
+++ SBOLDesigner/DrawView.swift
@@ -0,0 +1,89 @@
+//
+//  DrawView.swift
+//  SBOLDesigner
+//
+//  Created by Samuel Bridge on 11/5/18.
+//  Copyright © 2018 Samuel Bridge. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DrawView: UIView{
+    var lineColor: UIColor!
+    var lineWidth: CGFloat!
+    var path:UIBezierPath!
+    var touchPoint: CGPoint!
+    var startingPoint: CGPoint!
+    var currentGlyph: UIImage!
+    var layersToRemove = 0
+    @IBOutlet weak var drawOrGlyph: UISegmentedControl!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    @IBAction func clearClicked(_ sender: Any) {
+        clearCanvas()
+    }
+    
+    override func layoutSubviews() {
+        self.clipsToBounds = true
+        self.isMultipleTouchEnabled = false
+        self.clearButton.backgroundColor = UIColor.red
+        lineColor = UIColor.black
+        lineWidth = 5
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
+        if drawOrGlyph.selectedSegmentIndex == 0{
+            let imageView = UIImageView(image: currentGlyph)
+            imageView.frame = CGRect(x: startingPoint.x - (73/2), y: startingPoint.y - (128/2), width: 73, height: 128)
+            self.addSubview(imageView)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if drawOrGlyph.selectedSegmentIndex == 1{
+            let touch = touches.first
+            touchPoint = touch?.location(in: self)
+            
+            path = UIBezierPath()
+            path.move(to: startingPoint)
+            path.addLine(to: touchPoint)
+            startingPoint = touchPoint
+            
+            drawShapeLayer()
+        }
+    }
+    
+    func drawShapeLayer(){
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        self.layer.addSublayer(shapeLayer)
+        layersToRemove += 1
+        self.setNeedsDisplay()
+    }
+    
+    func clearCanvas(){
+        for view in self.subviews{
+            if let imageView = view as? UIImageView{
+                imageView.removeFromSuperview()
+            }
+        }
+        if(layersToRemove > 0){
+            path.removeAllPoints()
+            while layersToRemove > 0{
+                self.layer.sublayers?.remove(at: (self.layer.sublayers?.count)! - 1)
+                layersToRemove -= 1
+            }
+        }
+        self.setNeedsDisplay()
+    }
+    
+    func setCurrentGlyph(image: UIImage){
+        self.currentGlyph = image
+    }
+}
